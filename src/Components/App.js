582f3d0a5877411b5bce3a0daa56c6ed7e22ddef
natecrow <nate.crow76@gmail.com>
2017-09-09 18:13:07 -0400

    Clean up and re-arranging

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../style/App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Playlist</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
