3c8c100d0d717acc4ddc05bbfccb9c3c79d6cfe6
natecrow <nate.crow76@gmail.com>
2017-10-22 23:37:26 -0400

    Get all songs from playlist api through server

--- server/songs_router.js
+++ server/songs_router.js
@@ -1,13 +1,28 @@
+let axios = require('axios');
 let express = require('express');
 let router = express.Router();
 
+const PLAYLIST_SERVICE_URL = 'http://localhost:8080';
+
+// Get all songs
 router.get('/', (req, res) => {
-    res.send('GET route on songs');
-})
+
+    const playlistUrl = PLAYLIST_SERVICE_URL + '/songs';
+
+    return new Promise(() => {
+        axios.get(playlistUrl)
+            .then((response) => {
+                res.send(response.data);
+            })
+            .catch(error => {
+                res.send(error);
+            })
+    });
+});
 
 router.post('/', (req, res) => {
     res.send('POST route on songs');
-})
+});
 
 // set router to be the object returned from a require() call
 module.exports = router;
