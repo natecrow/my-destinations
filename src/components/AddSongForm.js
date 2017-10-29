d02d07e8ba3026c7ead21a682d3ae040fe9269b9
natecrow <nate.crow76@gmail.com>
2017-10-28 22:48:09 -0400

    Convert some components into functional components because they are presentational

--- src/components/AddSongForm.js
+++ src/components/AddSongForm.js
@@ -2,67 +2,60 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import PropTypes from 'prop-types';
 
-class AddSongForm extends React.Component {
-
-    render() {
-        const { handleSubmit, pristine, submitting } = this.props;
-
-        return (
-            <form onSubmit={handleSubmit}>
-                <h1>Add Song to Playlist</h1>
-                <div>
-                    <label>Title</label>
-                    <div>
-                        <Field name='title' component='input' type='text'
-                            placeholder='Title'
-                        />
-                    </div>
-                </div>
-                <div>
-                    <label>Artist</label>
-                    <div>
-                        <Field name='artist' component='input' type='text'
-                            placeholder='Artist'
-                        />
-                    </div>
-                </div>
-                <div>
-                    <label>Album</label>
-                    <div>
-                        <Field name='album' component='input' type='text'
-                            placeholder='Album'
-                        />
-                    </div>
-                </div>
-                <div>
-                    <label>Year</label>
-                    <div>
-                        <Field name='year' component='input' type='text'
-                            placeholder='Year'
-                        />
-                    </div>
-                </div>
-                <div>
-                    <label>Genre</label>
-                    <div>
-                        <Field name='genre' component='select'>
-                            <option />
-                            <option value='classical'>Classical</option>
-                            <option value='electronic'>Electronic</option>
-                            <option value='folk'>Folk</option>
-                            <option value='metal'>Metal</option>
-                            <option value='rock'>Rock</option>
-                            <option value='soundtrack'>Soundtrack</option>
-                        </Field>
-                    </div>
-                </div>
-                <div>
-                    <button type='submit' disabled={pristine || submitting}>Submit</button>
-                </div>
-            </form>
-        );
-    }
-};
+let AddSongForm = ({ handleSubmit, pristine, submitting }) => (
+    <form onSubmit={handleSubmit}>
+        <h1>Add Song to Playlist</h1>
+        <div>
+            <label>Title</label>
+            <div>
+                <Field name='title' component='input' type='text'
+                    placeholder='Title'
+                />
+            </div>
+        </div>
+        <div>
+            <label>Artist</label>
+            <div>
+                <Field name='artist' component='input' type='text'
+                    placeholder='Artist'
+                />
+            </div>
+        </div>
+        <div>
+            <label>Album</label>
+            <div>
+                <Field name='album' component='input' type='text'
+                    placeholder='Album'
+                />
+            </div>
+        </div>
+        <div>
+            <label>Year</label>
+            <div>
+                <Field name='year' component='input' type='text'
+                    placeholder='Year'
+                />
+            </div>
+        </div>
+        <div>
+            <label>Genre</label>
+            <div>
+                <Field name='genre' component='select'>
+                    <option />
+                    <option value='classical'>Classical</option>
+                    <option value='electronic'>Electronic</option>
+                    <option value='folk'>Folk</option>
+                    <option value='metal'>Metal</option>
+                    <option value='rock'>Rock</option>
+                    <option value='soundtrack'>Soundtrack</option>
+                </Field>
+            </div>
+        </div>
+        <div>
+            <button type='submit' disabled={pristine || submitting}>Submit</button>
+        </div>
+    </form>
+);
 
 AddSongForm.propTypes = {
     handleSubmit: PropTypes.any,
--- src/components/App.js
+++ src/components/App.js
@@ -4,21 +4,14 @@ import NavigationBar from './NavigationBar';
 import Home from './Home';
 import AddSongPage from '../containers/AddSongPage';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='App'>
-
-        <NavigationBar />
-
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/song' component={AddSongPage} />
-        </Switch>
-
-      </div>
-    );
-  }
-}
+const App = () => (
+  < div className='App' >
+    <NavigationBar />
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route path='/song' component={AddSongPage} />
+    </Switch>
+  </div >
+);
 
 export default App;
